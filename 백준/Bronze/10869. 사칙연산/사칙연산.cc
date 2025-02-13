06fdd59fa929415b204b0a0ea7a18f02a15f090a
taehan0 <taehan1000@naver.com>
2025-02-13 17:50:58 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    std::cin >> a;
+    std::cin >> b;
+
+    std::cout << a + b << std::endl
+              << a - b << std::endl
+              << a * b << std::endl
+              << a / b << std::endl
+              << a % b;
+
+    return 0;
+}
