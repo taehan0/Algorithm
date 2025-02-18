de169a7ff1659e0666ca048eca0327dd82d455b8
Taehan Kim <taehan1000@naver.com>
2025-02-18 16:49:12 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <map>
 using namespace std;
 int main()
 {
     cin.tie(0);
+    cout.tie(0);
     ios::sync_with_stdio(0);
-    int N, M;
+    int N, M, num;
     cin >> N;
-    map<int, int> m1;
-    int num;
-    for (int i = 0; i < N; i++)
+    vector<int> Arr(N);
+    for (int i = 0; i < N;i++)
     {
-        cin >> num;
-        m1[num] = 1;
+        cin >> Arr[i];
     }
-
+    sort(Arr.begin(), Arr.end());
     cin >> M;
     for (int i = 0; i < M;i++)
     {
         cin >> num;
-        cout << m1[num] << '\n';
+        if(binary_search(Arr.begin(), Arr.end(), num))
+        {
+            cout << 1 << '\n';
+        }
+        else
+            cout << 0 << '\n';
     }
-
-        return 0;
+    return 0;
 }
