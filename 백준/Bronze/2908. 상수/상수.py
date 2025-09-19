7a5dd4b15f080b09498b03ed838664f157df9f10
Taehan Kim <taehan1000@naver.com>
2025-09-19 13:36:08 +0900

    [Bronze II] Title: 상수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -13,8 +13,8 @@ for i in range(len(A)//2):
     B_list[i]=B_list[-i-1]
     B_list[-i-1]=temp
 
-A=int("".join(map(str,A_list)))
-B=int("".join(map(str,B_list)))
+A=int("".join(A_list))
+B=int("".join(B_list))
 
 if A>B:
     print(A)
